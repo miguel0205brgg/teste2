147b01d424d777604b25441b1c0e6cd7884117f4
miguel0205brgg <94112561+miguel0205brgg@users.noreply.github.com>
2025-11-11 04:47:55 -0500

    Fix: Configurando template_folder no Blueprint frontend_bp com caminho relativo corrigido

--- src/routes/frontend.py
+++ src/routes/frontend.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-frontend_bp = Blueprint("frontend", __name__)
+frontend_bp = Blueprint("frontend", __name__, template_folder="../../templates")
 
 @frontend_bp.route("/")
 def index():
